fd1bb57ce877f019c8e63a7a248b2d9fd06da558
prashanth <kasamsaimaniprashanth@gmail.com>
2026-04-07 22:42:49 +0530

    feat: implement user registration with verification email

--- app/types/ApiResponse.ts
+++ app/types/ApiResponse.ts
@@ -3,7 +3,6 @@ import { Message } from "../model/User";
 export interface ApiResponse {
     success: boolean;
     message: string;
-    status: number;
     isAccesptingMessages?: boolean;
     messages?: Array<Message>;
 }
